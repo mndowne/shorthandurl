9a76b81e073146cfc52a1816b4598632bd0d0f19
mndowne <mndowne@gmail.com>
2016-10-29 07:23:34 -0500

    got some regex down still need to add mongodb in:

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ var app = express();
 
 app.set('port',(process.env.PORT || 3000));
 
-app.get('/', function (req,res){
-  res.send("hello World");
+app.get('/4' , function (req,res){
 });
+
+app.get('/*', function (req,res){
+ 
+  var shorthand = "";
+
+  if (/http[s]?:\/{1,2}\w+\.[0-9a-zA-Z]+\.\w+/.test(req.params[0]) == false) 
+  {
+     req.params[0] = null;
+     shorthand = null;
+  }
+  else if (req.param[0] == //something in mongodb)
+  {
+    res.redirect(somethingfrom mongo);
+  }
+
+  var jsonOb = { original_url: req.params[0],
+                 shorthand_url: shorthand };
+  res.json(jsonOb);
+
+});
+
 
 app.listen(app.get('port'), function() {console.log('server is up on 3000 or the process.env.PORT');});
